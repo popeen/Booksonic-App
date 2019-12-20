2108adb978c308e520bcf85bc3cc75f2883df3f0
Patrik <patrik@ptjwebben.se>
2019-12-21 00:41:27 +0100

    Fixed incorrect url in demo server

--- app/src/main/java/github/popeen/dsub/activity/LoginActivity.java
+++ app/src/main/java/github/popeen/dsub/activity/LoginActivity.java
@@ -213,7 +213,7 @@ public class LoginActivity extends Activity {
 				editor.putBoolean(Constants.PREFERENCES_KEY_OFFLINE, false);
 
 				editor.putString(Constants.PREFERENCES_KEY_SERVER_NAME + 1, "Booksonic");
-				editor.putString(Constants.PREFERENCES_KEY_SERVER_URL + 1, "http://demo.booksonic.org");
+				editor.putString(Constants.PREFERENCES_KEY_SERVER_URL + 1, "http://demo.booksonic.org/booksonic/");
 				editor.putString(Constants.PREFERENCES_KEY_USERNAME + 1, "demo");
 				editor.putString(Constants.PREFERENCES_KEY_PASSWORD + 1, "demo");
 				editor.putInt(Constants.PREFERENCES_KEY_SERVER_INSTANCE, 1);
